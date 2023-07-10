30a46ace9402f859842b64b02796e973fa92c5bb
Aleksander Kotarski <olek.kotarski@gmail.com>
2023-07-10 19:08:15 +0200

    update

--- src/App/Exercises/ReactExercises/JakaLiczba/JakaLiczba.jsx
+++ src/App/Exercises/ReactExercises/JakaLiczba/JakaLiczba.jsx
@@ -4,28 +4,37 @@ import './style.css';
 
 export function JakaLiczba() {
   const [firstValue, setFirstValue] = useState(0);
+  const [randomNumber, setRandomNumber] = useState(0);
 
   const handleFirstValue = (e) => {
     let inputValue = e.target.value;
     setFirstValue(inputValue);
   };
 
+  function handleOnClick() {
+    const newRandomNumber = generateRandomNumber();
+    setRandomNumber(newRandomNumber);
+  }
   function generateRandomNumber() {
-    return Math.floor(Math.random() * 100) + 1;
+    return Math.floor(Math.random() * 10) + 1;
   }
 
-  let randomNumber = generateRandomNumber();
   console.log(randomNumber);
 
   return (
     <div>
       <h1>Jaką jestem liczbą</h1>
+      <button className="startButton" onClick={handleOnClick}>
+        START
+      </button>
+
+      <br></br>
       <input type="number" value={firstValue} onChange={handleFirstValue} />
       <p>
-        Podpowiedź:
+        Odpowiedź:
         {firstValue > randomNumber && 'Zgadywana liczba jest MNIEJSZA!'}
         {firstValue < randomNumber && 'Zgadtwana liczba jest WIĘKSZA!'}
-        {firstValue === randomNumber && 'NICE ONE, ZGADŁEŚ/AŚ'}
+        {firstValue == randomNumber && 'To ta liczba!'}
       </p>
     </div>
   );
