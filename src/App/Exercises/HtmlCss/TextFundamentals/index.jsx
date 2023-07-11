0274baf167d53527af84ad76eace18acbf52118a
Aleksander Kotarski <olek.kotarski@gmail.com>
2023-07-11 18:57:09 +0200

    update

---
+++ src/App/Exercises/HtmlCss/TextFundamentals/index.jsx
@@ -0,0 +1,38 @@
+import './styles.css';
+
+export function TextFundamentals() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+        distinctio iste dolor quos exercitationem praesentium, facilis
+        architecto laboriosam natus, dolores minus? Qui incidunt explicabo
+        deserunt veniam tenetur saepe exercitationem corporis?
+      </p>
+      <div className="blok-testowy">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+          distinctio iste dolor quos exercitationem praesentium, facilis
+          architecto laboriosam natus, dolores minus? Qui incidunt explicabo
+          deserunt veniam tenetur saepe exercitationem corporis?
+        </p>
+      </div>
+      <div className="blok-testowy-2">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+          distinctio iste dolor quos exercitationem praesentium, facilis
+          architecto laboriosam natus, dolores minus? Qui incidunt explicabo
+          deserunt veniam tenetur saepe exercitationem corporis?
+        </p>
+      </div>
+      <div className="blok-testowy-3">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+          distinctio iste dolor quos exercitationem praesentium, facilis
+          architecto laboriosam natus, dolores minus? Qui incidunt explicabo
+          deserunt veniam tenetur saepe exercitationem corporis?
+        </p>
+      </div>
+    </div>
+  );
+}
