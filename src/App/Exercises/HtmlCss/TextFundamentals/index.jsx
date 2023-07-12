b6fe95f29fc99e589ba63e87f992da828710bd9d
Aleksander Kotarski <olek.kotarski@gmail.com>
2023-07-12 17:25:49 +0200

    update

--- src/App/Exercises/HtmlCss/TextFundamentals/index.jsx
+++ src/App/Exercises/HtmlCss/TextFundamentals/index.jsx
@@ -1,15 +1,18 @@
+import React, { useState } from 'react';
 import './styles.css';
 
 export function TextFundamentals() {
+  const [font, setFont] = useState('no-font');
+
+  function handleChangeFont() {
+    setFont('fontQuickSand');
+  }
+  function defaultFont() {
+    setFont('text-block');
+  }
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
-        distinctio iste dolor quos exercitationem praesentium, facilis
-        architecto laboriosam natus, dolores minus? Qui incidunt explicabo
-        deserunt veniam tenetur saepe exercitationem corporis?
-      </p>
-      <div className="blok-testowy">
+    <div className="all">
+      <div className={`${setFont} ${font}`}>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
           distinctio iste dolor quos exercitationem praesentium, facilis
@@ -17,22 +20,8 @@ export function TextFundamentals() {
           deserunt veniam tenetur saepe exercitationem corporis?
         </p>
       </div>
-      <div className="blok-testowy-2">
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
-          distinctio iste dolor quos exercitationem praesentium, facilis
-          architecto laboriosam natus, dolores minus? Qui incidunt explicabo
-          deserunt veniam tenetur saepe exercitationem corporis?
-        </p>
-      </div>
-      <div className="blok-testowy-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
-          distinctio iste dolor quos exercitationem praesentium, facilis
-          architecto laboriosam natus, dolores minus? Qui incidunt explicabo
-          deserunt veniam tenetur saepe exercitationem corporis?
-        </p>
-      </div>
+      <button onClick={handleChangeFont}>Change font to QuickSand</button>
+      <button onClick={defaultFont}>Change to default</button>
     </div>
   );
 }
